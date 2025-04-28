f5e3be909d2c1b51a1bf593823a5717ce5c94f0e
ManishGhanshani <ghanshanimanish8@gmail.com>
2025-04-28 23:54:04 +0530

    emp_view_tasks, peronal_notes
    commit again

--- VgmsModules/Pms/emp_view_tasks.php
+++ VgmsModules/Pms/emp_view_tasks.php
@@ -366,7 +366,7 @@
 
                     <div class="d-flex align-items-center">
 
-                        <!-- Prev Button -->
+                        <!-- Prev Button --> 
                         <button class="page-link me-2 prev" data-list-pagination="prev" style="border-radius: 5px;">
                             <i class="fas fa-chevron-left"></i>
                         </button>
@@ -647,6 +647,5 @@
 </body>
 
 
-<!-- Mirrored from prium.github.io/phoenix/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 03 Sep 2024 13:37:21 GMT -->
 
 </html>
