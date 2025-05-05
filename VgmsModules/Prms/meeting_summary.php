55ee672cd524adae64574bf900bbf01e488aa199
NeavPanjwani <neavpanjwani.vg@gmail.com>
2025-05-05 23:50:38 +0530

    started with prms module and worked on 7 pages

---
+++ VgmsModules/Prms/meeting_summary.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr" data-navigation-type="default" data-navbar-horizontal-shape="default">
+
+
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Meeting Summary</title>
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="shortcut icon" type="image/x-icon" href="../../assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="../../assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="../../assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <script src="../../vendors/simplebar/simplebar.min.js"></script>
+    <script src="../../assets/js/config.js"></script>
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&amp;display=swap" rel="stylesheet">
+    <link href="../../vendors/simplebar/simplebar.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <link href="../../assets/css/theme-rtl.css" type="text/css" rel="stylesheet" id="style-rtl">
+    <link href="../../assets/css/theme.min.css" type="text/css" rel="stylesheet" id="style-default">
+    <link href="../../assets/css/user-rtl.min.css" type="text/css" rel="stylesheet" id="user-style-rtl">
+    <link href="../../assets/css/user.min.css" type="text/css" rel="stylesheet" id="user-style-default">
+    <script>
+        var phoenixIsRTL = window.config.config.phoenixIsRTL;
+        if (phoenixIsRTL) {
+            var linkDefault = document.getElementById('style-default');
+            var userLinkDefault = document.getElementById('user-style-default');
+            linkDefault.setAttribute('disabled', true);
+            userLinkDefault.setAttribute('disabled', true);
+            document.querySelector('html').setAttribute('dir', 'rtl');
+        } else {
+            var linkRTL = document.getElementById('style-rtl');
+            var userLinkRTL = document.getElementById('user-style-rtl');
+            linkRTL.setAttribute('disabled', true);
+            userLinkRTL.setAttribute('disabled', true);
+        }
+    </script>
+    <link href="../../vendors/leaflet/leaflet.css" rel="stylesheet">
+    <link href="../../vendors/leaflet.markercluster/MarkerCluster.css" rel="stylesheet">
+    <link href="../../vendors/leaflet.markercluster/MarkerCluster.Default.css" rel="stylesheet">
+    <link href="../../vendors/choices/choices.min.css" rel="stylesheet" />
+    <link href="../../vendors/flatpickr/flatpickr.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+        <?php include '../../Components/navbar.php'; ?>
+        <script>
+            var navbarTopShape = window.config.config.phoenixNavbarTopShape;
+            var navbarPosition = window.config.config.phoenixNavbarPosition;
+            var body = document.querySelector('body');
+            var navbarDefault = document.querySelector('#navbarDefault');
+            var navbarTop = document.querySelector('#navbarTop');
+            var topNavSlim = document.querySelector('#topNavSlim');
+            var navbarTopSlim = document.querySelector('#navbarTopSlim');
+            var navbarCombo = document.querySelector('#navbarCombo');
+            var navbarComboSlim = document.querySelector('#navbarComboSlim');
+            var dualNav = document.querySelector('#dualNav');
+
+            var documentElement = document.documentElement;
+            var navbarVertical = document.querySelector('.navbar-vertical');
+
+            if (navbarPosition === 'dual-nav') {
+                topNavSlim?.remove();
+                navbarTop?.remove();
+                navbarTopSlim?.remove();
+                navbarCombo?.remove();
+                navbarComboSlim?.remove();
+                navbarDefault?.remove();
+                navbarVertical?.remove();
+                dualNav.removeAttribute('style');
+                document.documentElement.setAttribute('data-navigation-type', 'dual');
+
+            } else if (navbarTopShape === 'slim' && navbarPosition === 'vertical') {
+                navbarDefault?.remove();
+                navbarTop?.remove();
+                navbarTopSlim?.remove();
+                navbarCombo?.remove();
+                navbarComboSlim?.remove();
+                topNavSlim.style.display = 'block';
+                navbarVertical.style.display = 'inline-block';
+                document.documentElement.setAttribute('data-navbar-horizontal-shape', 'slim');
+
+            } else if (navbarTopShape === 'slim' && navbarPosition === 'horizontal') {
+                navbarDefault?.remove();
+                navbarVertical?.remove();
+                navbarTop?.remove();
+                topNavSlim?.remove();
+                navbarCombo?.remove();
+                navbarComboSlim?.remove();
+                dualNav?.remove();
+                navbarTopSlim.removeAttribute('style');
+                document.documentElement.setAttribute('data-navbar-horizontal-shape', 'slim');
+            } else if (navbarTopShape === 'slim' && navbarPosition === 'combo') {
+                navbarDefault?.remove();
+                navbarTop?.remove();
+                topNavSlim?.remove();
+                navbarCombo?.remove();
+                navbarTopSlim?.remove();
+                dualNav?.remove();
+                navbarComboSlim.removeAttribute('style');
+                navbarVertical.removeAttribute('style');
+                document.documentElement.setAttribute('data-navbar-horizontal-shape', 'slim');
+            } else if (navbarTopShape === 'default' && navbarPosition === 'horizontal') {
+                navbarDefault?.remove();
+                topNavSlim?.remove();
+                navbarVertical?.remove();
+                navbarTopSlim?.remove();
+                navbarCombo?.remove();
+                navbarComboSlim?.remove();
+                dualNav?.remove();
+                navbarTop.removeAttribute('style');
+                document.documentElement.setAttribute('data-navigation-type', 'horizontal');
+            } else if (navbarTopShape === 'default' && navbarPosition === 'combo') {
+                topNavSlim?.remove();
+                navbarTop?.remove();
+                navbarTopSlim?.remove();
+                navbarDefault?.remove();
+                navbarComboSlim?.remove();
+                dualNav?.remove();
+                navbarCombo.removeAttribute('style');
+                navbarVertical.removeAttribute('style');
+                document.documentElement.setAttribute('data-navigation-type', 'combo');
+            } else {
+                topNavSlim?.remove();
+                navbarTop?.remove();
+                navbarTopSlim?.remove();
+                navbarCombo?.remove();
+                navbarComboSlim?.remove();
+                dualNav?.remove();
+                navbarDefault.removeAttribute('style');
+                navbarVertical.removeAttribute('style');
+            }
+
+            var navbarTopStyle = window.config.config.phoenixNavbarTopStyle;
+            var navbarTop = document.querySelector('.navbar-top');
+            if (navbarTopStyle === 'darker') {
+                navbarTop.setAttribute('data-navbar-appearance', 'darker');
+            }
+
+            var navbarVerticalStyle = window.config.config.phoenixNavbarVerticalStyle;
+            var navbarVertical = document.querySelector('.navbar-vertical');
+            if (navbarVerticalStyle === 'darker') {
+                navbarVertical.setAttribute('data-navbar-appearance', 'darker');
+            }
+        </script>
+        <div class="content">
+            <h3>Meeting Summary</h3>
+            <h6>Record or review meetings related to proposals, including participants, agenda, and key notes.</h6>
+            <hr>
+
+            <form method="POST" action="#" class="form">
+
+                <div class="form-group" id="field-previous-meetings">
+                    <label class="form-label" style="font-size: 13px;">Previous Meetings</label>
+
+                    <div id="meetingList" data-list='{"valueNames":["date","participants","agenda","notes"],"page":5,"pagination":{"innerWindow":1,"left":1,"right":1}}'>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-sm fs-9 mb-0">
+                                <thead>
+                                    <tr>
+                                        <th class="sort border-top ps-3" data-sort="date">Date</th>
+                                        <th class="sort border-top" data-sort="participants">Participants</th>
+                                        <th class="sort border-top" data-sort="agenda">Agenda</th>
+                                        <th class="sort border-top" data-sort="notes">Notes</th>
+                                        <th class="border-top text-end pe-0">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="list">
+                                    <tr>
+                                        <td class="align-middle ps-3 date">2025-05-01</td>
+                                        <td class="align-middle participants">John, Jane</td>
+                                        <td class="align-middle agenda">Proposal finalization</td>
+                                        <td class="align-middle notes">Reviewed client feedback and finalized changes.</td>
+                                        <td class="align-middle text-end pe-0">
+                                            <div class="btn-reveal-trigger position-static">
+                                                <button class="btn btn-sm dropdown-toggle btn-reveal fs-10" type="button" data-bs-toggle="dropdown">
+                                                    <i class="fas fa-ellipsis-h"></i>
+                                                </button>
+                                                <div class="dropdown-menu dropdown-menu-end py-2">
+                                                    <a class="dropdown-item" href="#">View</a>
+                                                    <a class="dropdown-item" href="#">Export</a>
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item text-danger" href="#">Delete</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <!-- Add more meeting rows here using the same structure -->
+
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="d-flex justify-content-center mt-3">
+                            <button class="page-link" data-list-pagination="prev">‹</button>
+                            <ul class="mb-0 pagination"></ul>
+                            <button class="page-link" data-list-pagination="next">›</button>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-6 ">
+                        <!-- Meeting Date -->
+                        <div class="form-group" id="field-meeting-date">
+                            <label class="form-label">Meeting Date</label>
+                            <input class="form-control datetimepicker flatpickr-input" id="datetimepicker" type="text" placeholder="dd/mm/yyyy hour : minute" data-options="{&quot;enableTime&quot;:true,&quot;dateFormat&quot;:&quot;d/m/y H:i&quot;,&quot;disableMobile&quot;:true}" readonly="readonly">
+
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <!-- Participants -->
+                        <label class="form-label">Participants</label>
+                        <select class="form-select" id="organizerMultiple" data-choices="data-choices" multiple="multiple" data-options='{"removeItemButton":true,"placeholder":true}'>
+                            <option value="">Select organizer...</option>
+                            <option>Massachusetts Institute of Technology</option>
+                            <option>University of Chicago</option>
+                            <option>GSAS Open Labs At Harvard</option>
+                            <option>California Institute of Technology</option>
+                        </select>
+
+                    </div>
+
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-6 ">
+                        <!-- Agenda -->
+                        <div class="form-group" id="field-agenda">
+                            <label class="form-label">Agenda</label>
+                            <textarea name="agenda" class="form-control" rows="3" placeholder="Enter meeting agenda..." required></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <!-- Notes -->
+                        <div class="form-group" id="field-notes">
+                            <label class="form-label">Meeting Notes</label>
+                            <textarea name="notes" class="form-control" rows="4" placeholder="Write key points or next steps..." required></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <!-- Submit Button -->
+                <div class="form-group" id="field-submit" style="text-align: right;">
+                    <button type="submit" class="btn btn-primary">Save Summary</button>
+                </div>
+
+            </form>
+            <!-- <div class="form-group" id="field-previous-meetings">
+                <label class="form-label">Previous Meetings</label>
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Participants</th>
+                            <th>Agenda</th>
+                            <th>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2025-05-01</td>
+                            <td>John, Jane</td>
+                            <td>Proposal finalization</td>
+                            <td>Reviewed client feedback and finalized changes.</td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div> -->
+
+            <div class="c" style="margin-top: 1%;">
+                <?php include("../../Components/footer.php"); ?>
+            </div>
+        </div>
+
+
+
+
+
+
+    </main>
+
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+
+    <!-- ===============================================-->
+    <!--Dropdown for contacted-to--->
+
+    <script src="../../vendors/popper/popper.min.js"></script>
+    <script src="../../vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="../../vendors/anchorjs/anchor.min.js"></script>
+    <script src="../../vendors/is/is.min.js"></script>
+    <script src="../../vendors/fontawesome/all.min.js"></script>
+    <script src="../../vendors/lodash/lodash.min.js"></script>
+    <script src="../../vendors/list.js/list.min.js"></script>
+    <script src="../../vendors/feather-icons/feather.min.js"></script>
+    <script src="../../vendors/dayjs/dayjs.min.js"></script>
+    <script src="../../vendors/leaflet/leaflet.js"></script>
+    <script src="../../vendors/leaflet.markercluster/leaflet.markercluster.js"></script>
+    <script src="../../vendors/leaflet.tilelayer.colorfilter/leaflet-tilelayer-colorfilter.min.js"></script>
+    <script src="../../assets/js/phoenix.js"></script>
+    <script src="../../vendors/echarts/echarts.min.js"></script>
+    <script src="../../assets/js/ecommerce-dashboard.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+    <script>
+    // Select all form fields inside the form
+    const fields = document.querySelectorAll(".form .form-group");
+
+    // Animate each form-group with staggered delay
+    fields.forEach((field, index) => {
+        gsap.from(field, {
+            opacity: 0,
+            y: 40,
+            duration: 0.5,
+            delay: 0.1 * index,
+            ease: "power2.out"
+        });
+    });
+</script>
+
+
+    <script src="../../vendors/choices/choices.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js"></script>
+    <script src="../../vendors/flatpickr/flatpickr.min.js"></script>
+
+
+
+
+
+
+    </script>
+
+
+
+</body>
+
+
+<!-- Mirrored from prium.github.io/phoenix/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 03 Sep 2024 13:37:21 GMT -->
+
+</html>
